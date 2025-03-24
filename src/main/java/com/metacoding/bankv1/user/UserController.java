876f5466b4ec952ddd1a8d549551ef4d242805b0
getinthere <getinthere@naver.com>
2025-03-24 17:37:24 +0900

    join complete

---
+++ src/main/java/com/metacoding/bankv1/user/UserController.java
@@ -0,0 +1,29 @@
+package com.metacoding.bankv1.user;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login-form")
+    public String loginForm() {
+        return "user/login-form";
+    }
+
+    @GetMapping("/join-form")
+    public String joinForm() {
+        return "user/join-form";
+    }
+
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO joinDTO, HttpServletRequest request) {
+        userService.회원가입(joinDTO);
+        return "redirect:/login-form";
+    }
+}
---
+++ src/main/java/com/metacoding/bankv1/user/UserRequest.java
@@ -0,0 +1,19 @@
+package com.metacoding.bankv1.user;
+
+import lombok.Data;
+
+public class UserRequest {
+
+    @Data
+    public static class JoinDTO {
+        private String username;
+        private String password;
+        private String fullname;
+    }
+
+    @Data
+    public static class LoginDTO {
+        private String username;
+        private String password;
+    }
+}
