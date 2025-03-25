fc9dd751b8b6846aa11fa8f99aa83c7279c69ca3
son <sssk1755@gmail.com>
2025-03-25 12:38:50 +0900

    ãsave1

--- src/main/java/com/metacoding/bankv1/user/UserController.java
+++ src/main/java/com/metacoding/bankv1/user/UserController.java
@@ -1,6 +1,6 @@
 package com.metacoding.bankv1.user;
 
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +10,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
     private final UserService userService;
+    private final HttpSession session;
+
+
+    // 로그인만 예외로 @Post (조회시에도)
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO loginDTO) {
+        User sessionUser = userService.로그인(loginDTO);
+        session.setAttribute("sessionUser", sessionUser); //stateful
+        return "redirect:/";
+    }
 
     @GetMapping("/login-form")
     public String loginForm() {
         return "user/login-form";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate();
+        return "redirect:/";
     }
 
     @GetMapping("/join-form")
@@ -22,7 +38,7 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    public String join(UserRequest.JoinDTO joinDTO, HttpServletRequest request) {
+    public String join(UserRequest.JoinDTO joinDTO) {
         userService.회원가입(joinDTO);
         return "redirect:/login-form";
     }
--- src/main/java/com/metacoding/bankv1/user/UserService.java
+++ src/main/java/com/metacoding/bankv1/user/UserService.java
@@ -20,4 +20,23 @@ public class UserService {
         // 3. 없으면 회원가입하기
         userRepository.save(joinDTO.getUsername(), joinDTO.getPassword(), joinDTO.getFullname());
     }
+
+    public User 로그인(UserRequest.LoginDTO loginDTO) {
+        //1. 해당 유저네임이 있나?
+        User user = userRepository.findByUsername(loginDTO.getUsername());
+
+        //2. 필터링(유저네임, 패스워드가 불일치하는 것들을)
+        if (user == null) {
+            throw new RuntimeException("해당 username이 없습니다");
+        }
+
+        if (!(user.getPassword().equals(loginDTO.getPassword()))) {
+            throw new RuntimeException("해당 password가 틀렸습니다");
+
+        }
+
+        //3. 인증
+        return user;
+
+    }
 }
